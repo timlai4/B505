47ca87d8ba84fce6e2f5d972d9d8a1016530a26f
Tim <46388504+timlai4@users.noreply.github.com>
2019-01-19 21:43:29 -0500

    Update intersect.py

--- intersect.py
+++ intersect.py
@@ -1,16 +1,16 @@
+intersect = []
 def intersection(list1, list2):
-  # Finding the intersection of two sorted lists
-  if list1 or list2:
-    return null
-  intersect = []
-  M = len(list1);
-  N = len(list2);
-  for element in list1:
-    if element = list2[N//2]
-      intersect.append(element)
-    if element > list2[N//2]:
-      intersection(list1,list2[N//2:]
-    if element < list2[N//2]:
-      intersection(list1,list2[:N//2+1]
-
+  # Finding the intersection of two sorted lists 
+  N = len(list2)
+  if N > 0:
+    for element in list1:
+      if element == list2[N//2]:
+        intersect.append(element)
+      if element > list2[N//2]:
+        intersection([element],list2[N//2-1:])
+      if element < list2[N//2]:
+        intersection([element],list2[:N//2])          
   return intersect
+list1=[1,2,3]
+list2=[1,2,3]
+print(intersection(list1,list2))
